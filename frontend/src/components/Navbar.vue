6eca1244e2b829876484499c761ba152e7b56f6f
mtamb <mtambomonde@hotmail.com>
2026-03-21 04:23:45 +0200

    fix(notifications): toggle notification & user menus on click without being closed by outside click

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -33,7 +33,7 @@
       <div class="topbar-right">
 
         <div class="notif-wrapper" ref="notifWrapper">
-          <button class="topbar-icon-btn" title="Notifications" @click="toggleNotifications">
+          <button class="topbar-icon-btn" title="Notifications" @click.stop="toggleNotifications">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg>
             <span v-if="unreadCount > 0" class="notif-badge">{{ unreadCount > 9 ? '9+' : unreadCount }}</span>
           </button>
@@ -70,7 +70,7 @@
         </div>
 
         <div class="user-menu" ref="userWrapper">
-          <button class="user-btn" @click="toggleUserMenu">
+          <button class="user-btn" @click.stop="toggleUserMenu">
             <div class="user-avatar-sm">{{ userInitials }}</div>
             <div class="user-info-sm">
               <span class="user-name-sm">{{ userFullName }}</span>
